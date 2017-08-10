132a076f613a4e9d0b5b14314ba62fd8b3c7f35b
or ferzdhant <ohrf02@gmail.com>
2017-08-10 23:06:41 +0300

    The Game Of Life

---
+++ lifev2/cmake-build-debug/CMakeFiles/lifev2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lifev2.dir/main.c.obj"
+  "lifev2.pdb"
+  "lifev2.exe"
+  "lifev2.exe.manifest"
+  "liblifev2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lifev2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
